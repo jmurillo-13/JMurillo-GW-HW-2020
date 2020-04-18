6f77a46c44bd7a334c453f99f56a15c903e27cfb
jmurillo-13 <61661469+jmurillo-13@users.noreply.github.com>
2020-04-18 14:46:31 -0400

    Python Challenges and Pandas

--- Python Module/Python Challenges/PyBank/main.py
+++ Python Module/Python Challenges/PyBank/main.py
@@ -6,85 +6,86 @@ Created on Mon Apr 13 22:24:32 2020
 @author: murillojessica
 """
 
+#import dependencies
 import os
 import csv
 
-budget_csv = os.path.join("Resources", "budget_data.csv")
+#File Path
+budget_csv = os.path.join('Resources', 'budget_data.csv')
 
+#Variables
 total_months = 0
 total_revenue = 0
-previous_revenue = 0
-months=[]
-profit=[]
-revenue_change= []
+previous_value = 0
+revenue_dif = 0
+min_dif= 0
+max_dif =0
+rev_dif_list=[]
 
+
+#opening CSV File 
 with open(budget_csv) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    csvreader = next (csvreader, None)
+    csvreader = csv.reader(csvfile, delimiter = ',') 
+    csv_header = next(csvreader)
 
     for row in csvreader:
         date_column = row[0]
-        profit_column = int(row[1])
-
+        revenue_column = row[1]
+        revenue_column_int = int(revenue_column)
+        
+        #Calculate the difference between two rows
+        revenue_dif= revenue_column_int - previous_value
+        previous_value = revenue_column_int
+        
+        #Capture the differences in alist
+        rev_dif_list = rev_dif_list + [revenue_dif] 
+        
+        
+        if max_dif < revenue_dif:
+            max_dif = revenue_dif
+            max_date = date_column
+            
+        if min_dif > revenue_dif:
+            min_dif = revenue_dif
+            min_date = date_column
+            
+        previous_value = revenue_column_int
+        #Count Month
         total_months = total_months + 1
-        total_revenue = total_revenue + profit_column
         
-        revenue_change = profit_columnn-previous_revenue
+        #Sum net revenue 
+        total_revenue = total_revenue + revenue_column_int  
+        revenue_avg = round(sum(rev_dif_list)/len(rev_dif_list))
+        
+        
+        
+#Print Results
+output =(f'Financial Analysis\n'
+         f'-----------------------------------------------------\n'   
+         f'Total Months : {total_months}\n'
+         f'Total: $ {total_revenue}\n'
+         f'Average Change: {revenue_avg}\n'
+         f'Greatest Increase in Profits: {max_date} : $ ({max_dif})\n'
+         f'Greatest Decrease in Profits: {min_date} : ($ {min_dif})\n')
+print(output)
+
+#Export to TXT File 
+file_to_output = os.path.join("Analysis", "final.txt")
+with open(file_to_output, "w") as txt_file:
+    txt_file.write(output)
 
 
 
-
-
-
-
-
-
-'''
-        months.append(date_comumn)
-        profit.append(profit_column)
-
-       
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
- """    
-
-
-     months.append(date_comumn)
-        profit.append(profit_column)   
-       revenue_chnage[]
-       
-    for x in something :
-        
-        revenu_change = 
-        
-        total_profit = 
-        total_months =len(months)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        #print(total_profit)
-      
-        #def max_num_in_list( list ):
-2	    #max = list[ 0 ]
-3	    #for a in list:
-4	       # if a > max:
-5	           # max = a
-6	    #return max
-     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
